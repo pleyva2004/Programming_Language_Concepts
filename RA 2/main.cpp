b5ba991a86c7b843db0f37f43faa6cd3ee8b6d33
Pablo <pleyva2004@gmail.com>
2024-01-29 11:09:24 -0500

    Fixing Jason files

--- RA 2/main.cpp
+++ RA 2/main.cpp
@@ -17,3 +17,32 @@ int character; // use int, because char cannot represent EOF
 
     return 0;
 }
+
+
+
+{
+                  "name": "C/C++: g++.exe build and debug active file",
+                  "type": "cppdbg",
+                  "request": "launch",
+                  "program": "${fileDirname}\\${fileBasenameNoExtension}.exe",
+                  "args": [],
+                  "stopAtEntry": false,
+                  "cwd": "${fileDirname}",
+                  "environment": [],
+                  "externalConsole": false,
+                  "MIMode": "gdb",
+                  "miDebuggerPath": "C:\\Coding\\mingw64\\bin\\gdb.exe",
+                  "setupCommands": [
+                        {
+                              "description": "Enable pretty-printing for gdb",
+                              "text": "-enable-pretty-printing",
+                              "ignoreFailures": true
+                        },
+                        {
+                              "description": "Set Disassembly Flavor to Intel",
+                              "text": "-gdb-set disassembly-flavor intel",
+                              "ignoreFailures": true
+                        }
+                  ],
+                  "preLaunchTask": "C/C++: g++.exe build active file"
+            }
